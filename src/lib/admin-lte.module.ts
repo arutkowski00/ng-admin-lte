d85c3ea01e00b0c5199f7ff5b8b356612e0b6516
Adam Rutkowski <a_rutkowski@outlook.com>
2017-12-03 17:15:31 +0100

    chore: add hello component

--- src/lib/admin-lte.module.ts
+++ src/lib/admin-lte.module.ts
@@ -1,12 +1,18 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HelloComponent } from './hello.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [],
-  exports: []
+  declarations: [
+    HelloComponent
+  ],
+  exports: [
+    HelloComponent
+  ]
 })
 export class AdminLteModule {
   static forRoot(): ModuleWithProviders {
---
+++ src/lib/hello.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lte-hello',
+  template: `<h1>Hello World!</h1>`
+})
+
+export class HelloComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
